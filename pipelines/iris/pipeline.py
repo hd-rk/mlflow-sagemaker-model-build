57215077a8a7bf6ded67d795a085d9ab59142cd2
hong <hdn2@sfu.ca>
2023-02-07 08:01:47 +0000

    fix: pipeline

--- pipelines/iris/pipeline.py
+++ pipelines/iris/pipeline.py
@@ -212,6 +212,7 @@ def get_pipeline(
         instance_type=training_instance_type,
         framework_version='0.23-1',
         base_job_name=f"{base_job_prefix}/sklearn-iris-train",
+        sagemaker_session=pipeline_session,
         disable_profiler=True
     )
     
